050d3bbeeea575f5f6d67a6e2b47c45c14aa7d4c
Ruzman <roesman.labfik@umi.ac.id>
2023-12-03 16:49:56 +0800

    beta 0.1.0

---
+++ resources/views/skpd/add.blade.php
@@ -0,0 +1,52 @@
+Hosting Segera Berkinerja
+
+ssh key
+vF;cP$4cK4^^
+
+cpanel_key
+&T?@t)Mp#=VN
+
+Generating public/private rsa key pair.
+Enter passphrase (empty for no passphrase):
+Enter same passphrase again:
+Your identification has been saved in /home/dhfghlcg/.ssh/id_rsa.
+Your public key has been saved in /home/dhfghlcg/.ssh/id_rsa.pub.
+The key fingerprint is:
+SHA256:+X4UZ9I5lyXc7dp5Z/auIdjT6uWzw3A34oYub9fdt84
+The key's randomart image is:
++---[RSA 2048]----+
+|             . ..|
+|              o +|
+|             . +o|
+|         .  o *.o|
+|        S    = =.|
+|         . oooooB|
+|          o.*+*+B|
+|         o o.X=o=|
+|          *== =E+|
++----[SHA256]-----+
+
+cpanel_key
+Generating public/private rsa key pair.
+Enter passphrase (empty for no passphrase):
+Enter same passphrase again:
+Your identification has been saved in /home/dhfghlcg/.ssh/cpanel_key.
+Your public key has been saved in /home/dhfghlcg/.ssh/cpanel_key.pub.
+The key fingerprint is:
+SHA256:uFGA5PrnLU+gFQ2EBvjJYNlfu0SCwrj2bhEYZhe+9U8
+The key's randomart image is:
++---[RSA 4096]----+
+|o.+++++          |
+|+O.=+. *         |
+|+==+o.= +        |
+|.o+oo..*         |
+|. o.. *.SE       |
+|   + o =o        |
+|  . + o ..       |
+|   o o.o         |
+|  .   .oo        |
++----[SHA256]-----+
+
+
+db
+,0ZzR_k,zJVL^ht$+1
