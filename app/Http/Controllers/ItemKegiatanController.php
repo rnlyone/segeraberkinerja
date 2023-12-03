94312580da4b2d21843281fce187738b3da9a294
Ruzman <roesman.labfik@umi.ac.id>
2023-12-03 21:54:03 +0800

    beta 0.1.1 : fungsi program rutin otomatis, fix bug

--- app/Http/Controllers/ItemKegiatanController.php
+++ app/Http/Controllers/ItemKegiatanController.php
@@ -281,7 +281,7 @@ class ItemKegiatanController extends Controller
             return redirect()->route('kegiatan.show', ['kegiatan' => $kegiatan->id])->with('success', 'Sub Kegiatan Berhasil di Edit');
 
         } catch (\Throwable $th) {
-            dd($th);
+            return redirect()->back()->with('error', 'Sub Kegiatan Tidak Berhasil di Edit');
         }
     }
 
